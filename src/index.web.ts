c2d61267366de97da49bd6fd997e20f2d4793257
rofi <rfigueroa@truelogic.io>
2023-10-17 15:57:08 -0300

    feat: use export * syntax

--- src/index.web.ts
+++ src/index.web.ts
@@ -1,8 +1,1 @@
-// export * from "../dist/index.js";
-// export * from "./use-chat";
-// export * from "./use-completion";
-
-// export * from 'ai/react/dist/index.js';
-import { useChat, useCompletion } from 'ai/react/dist';
-
-export { useChat, useCompletion };
+export * from 'ai/react/dist';
